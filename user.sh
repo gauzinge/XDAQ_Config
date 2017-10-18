d7c91cecfd326d8c5912947d412e41f1b4ed045e
G. Auzinger <georg.auzinger@cern.ch>
2017-10-18 22:43:22 +0200

    some initial changes, in the first inline config file

---
+++ user.sh
@@ -0,0 +1,14 @@
+export JAVA_HOME=/usr/java/jdk1.8.0_151 
+export CATALINA_HOME=/usr/local/apache-tomcat-8.0.22
+
+## RCMS
+export ANT_HOME=/opt/apache-ant-1.6.5
+export RCMS_HOME=/opt/TriDAS/RunControl
+export PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$CATALINA_HOME:$PATH
+
+#############################################################################
+# Aliases
+#############################################################################
+alias  StartTomcat="$CATALINA_HOME/bin/startup.sh"
+alias  StopTomcat="$CATALINA_HOME/bin/shutdown.sh"
+#alias  RunManager="java -Duser.home=/home/xtaldaq/RCMS -jar /home/xtaldaq/RCMS/manager.jar"
